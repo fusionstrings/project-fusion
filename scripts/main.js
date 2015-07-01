9db7f51b5ae16765d22a8c5f96528365753ce066
Dilip Kr. Shukla <contact@fusionstrings.com>
2015-07-01 23:07:26 +0530

    2nd commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 import {bootstrap} from './es-mod';
-import s from './common-mod';
+import cjs from './common-mod';
 bootstrap();
-var d = s();
-console.log(d);
+var cjsImport = cjs();
+console.log(cjsImport, 'CommonJS');
 //console.log( 'The area of a circle of radius 4 is ' + s);
